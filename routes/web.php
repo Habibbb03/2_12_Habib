3947e42b6d63a5fe434cf672a627e0cb4e3cf479
Habibbb03 <habibrafii0312@gmail.com>
2022-06-07 18:49:34 +0700

    modul 5-7

---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index',[
+        "title" => "Home",
+        "nama" => "Habib",
+        "email" => "habib@yahoo.co.id",
+        "image" => "img.jpg"
+    ]);
+});
+Route::get('/about', function () {
+    return view('about',[
+        "title" => "About",
+        "nama" => "Habib",
+        "email" => "habib@yahoo.co.id",
+        "image" => "img.jpg"
+    ]);
+});
+Route::get('/gallery', function () {
+    return view('gallery',[
+        "title" => "Gallery"
+    ]); 
+});
+//  Route::resource('contact',ContactController::class);
+
+Route::get('/contact/create', [ContactController::class, 'create'])->name('contact.create');
+Route::post('/contact/store', [ContactController::class, 'store'])->name('contact.store');
+
+Auth::routes();
+
+Route::group(['middleware' =>['auth']], function (){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/contact/index', [ContactController::class, 'index'])->name('contact.index');
+    Route::get('/contact/{id}/edit', [ContactController::class, 'edit'])->name('contact.edit');
+    Route::post('/contact/{id}/update', [ContactController::class, 'update'])->name('contact.update');
+    Route::get('/contact/{id}/destroy', [ContactController::class, 'destroy'])->name('contact.destroy');
+});
